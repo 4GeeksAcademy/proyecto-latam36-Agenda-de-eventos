9179df9bf31f072bacfbb06e06f5ba1449b38a95
Anajerar <aurelio.najerar@gmail.com>
2024-12-12 03:43:41 +0000

    Cambios de prueba

--- src/api/models.py
+++ src/api/models.py
@@ -5,8 +5,18 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password_hash = db.Column(db.String(80), unique=False, nullable=False)
+    user_first_name = db.Column(db.String(100), unique=False, nullable=True)
+    user_last_name = db.Column(db.String(100), unique=False, nullable=True)
+    user_city = db.Column(db.String(30), unique=False, nullable=True)
+    user_state = db.Column(db.String(25), unique=False, nullable=True)
+    user_country = db.Column(db.String(25), unique=False, nullable=True)
+    user_role = db.Column(db.String(15), unique=False, nullable=True)
+    user_date_of_birth = db.Column(db.Date, unique=False, nullable=True)
+    user_genre = db.Column(db.String(10), unique=False, nullable=True)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=True)
+    is_event_organizer = db.Column(db.Boolean(), unique=False, nullable=True)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
