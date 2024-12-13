3037720e19c17e6cbafd1855bea13ef692946c29
Anajerar <aurelio.najerar@gmail.com>
2024-12-13 23:37:10 +0000

    Revert "."
    This reverts commit 66161cd353149a658fd9e94dc3cc404569d50b27, reversing
    changes made to 3e0a9cf7e24877385750888cc9caf4a51a4793c6.

--- src/api/models.py
+++ src/api/models.py
@@ -5,10 +5,12 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(250), unique=False, nullable=False)
+    username = db.Column(db.String(15))
+    password_hash = db.Column(db.String(80), unique=False, nullable=False)
     first_name = db.Column(db.String(100), unique=False, nullable=True)
     last_name = db.Column(db.String(100), unique=False, nullable=True)
     user_city = db.Column(db.String(30), unique=False, nullable=True)
+    user_state = db.Column(db.String(25), unique=False, nullable=True)
     user_country = db.Column(db.String(25), unique=False, nullable=True)
     user_role = db.Column(db.String(15), unique=False, nullable=True)
     birthdate = db.Column(db.Date, unique=False, nullable=True)
@@ -20,36 +22,17 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.email}>'
 
-    def __init__(self, email, password_hash, first_name=None, last_name=None, genre=None, birthdate=None, city=None, country=None):
+    def __init__(self, email, password_hashed):
         self.email = email
-        self.password_hash = password_hash
-        self.first_name = first_name
-        self.last_name = last_name
-        self.genre = genre
-        self.birthdate = birthdate
-        self.city = city
-        self.country = country
-        self.role = "User"  # Default role
+        self.password_hash = password_hashed
+        self.role = None
         self.is_admin = False
         self.is_event_organizer = False
         self.is_active = True
 
     def serialize(self):
-        role = "User"
-        if self.is_admin:
-            role = "Admin"
-        elif self.is_event_organizer:
-            role = "Event Organizer"
-
         return {
             "id": self.id,
             "email": self.email,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "country": self.country,
-            "city": self.city,
-            "genre": self.genre,
-            "date_of_birth": self.birthdate.strftime("%d/%m/%Y") if self.birthdate else None,
-            "role": role
+            "role": self.role
         }
-
