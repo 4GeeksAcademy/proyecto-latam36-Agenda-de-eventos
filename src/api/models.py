ce4a2caf3ffbdf856cd99e5361d1cf41b67fa2e9
martincorestein <103467287+martincorestein@users.noreply.github.com>
2024-12-13 21:41:38 +0000

    elimino username y state del modelo y del cconstructor, no los vamos a usar

--- src/api/models.py
+++ src/api/models.py
@@ -5,12 +5,10 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(15))
     password_hash = db.Column(db.String(250), unique=False, nullable=False)
     first_name = db.Column(db.String(100), unique=False, nullable=True)
     last_name = db.Column(db.String(100), unique=False, nullable=True)
     user_city = db.Column(db.String(30), unique=False, nullable=True)
-    user_state = db.Column(db.String(25), unique=False, nullable=True)
     user_country = db.Column(db.String(25), unique=False, nullable=True)
     user_role = db.Column(db.String(15), unique=False, nullable=True)
     birthdate = db.Column(db.Date, unique=False, nullable=True)
@@ -22,7 +20,7 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.email}>'
 
-    def __init__(self, email, password_hash, first_name=None, last_name=None, genre=None, birthdate=None, city=None, state=None, country=None):
+    def __init__(self, email, password_hash, first_name=None, last_name=None, genre=None, birthdate=None, city=None, country=None):
         self.email = email
         self.password_hash = password_hash
         self.first_name = first_name
@@ -30,7 +28,6 @@ class User(db.Model):
         self.genre = genre
         self.birthdate = birthdate
         self.city = city
-        self.state = state
         self.country = country
         self.role = "User"  # Default role
         self.is_admin = False
@@ -50,7 +47,6 @@ class User(db.Model):
             "first_name": self.first_name,
             "last_name": self.last_name,
             "country": self.country,
-            "state": self.state,
             "city": self.city,
             "genre": self.genre,
             "date_of_birth": self.birthdate.strftime("%d/%m/%Y") if self.birthdate else None,
