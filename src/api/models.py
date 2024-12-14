c02da45c21d2f076ee2756405323f2270dad6903
Anajerar <aurelio.najerar@gmail.com>
2024-12-14 00:07:25 +0000

    Nueva version de la base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -5,7 +5,6 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(15))
     password_hash = db.Column(db.String(80), unique=False, nullable=False)
     first_name = db.Column(db.String(100), unique=False, nullable=True)
     last_name = db.Column(db.String(100), unique=False, nullable=True)
