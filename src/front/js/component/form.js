d9dc91c8766b697468c7a49b4bf590887762c146
andreinapazz <108377491+andreinapazz@users.noreply.github.com>
2025-01-08 06:11:35 +0000

    arreglo estilos

--- src/front/js/component/form.js
+++ src/front/js/component/form.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import "../../styles/eventForm.css";
 
 const Form = () => {
@@ -20,11 +21,12 @@ const Form = () => {
   country: 'Colombia',
   instagramEventName: '', 
   facebookEventName: '',  
-  priceUSD: ''    
+  isFree: false,
+  priceUSD: '', 
  });
 
  const handleChange = (e) => {
- const { name, value } = e.target;
+ const { name, value, type, checked } = e.target;
 
  if (name.startsWith('socialMedia.')) {
    setFormData((prevState) => ({
@@ -37,19 +39,19 @@ const Form = () => {
    return;
  }
 
- if (name === 'isOnlineEvent') {
-   setFormData((prevState) => ({
-     ...prevState,
-     isOnlineEvent : e.target.checked
-   }));
-   return;
- }
-
- setFormData((prevState) => ({
+ if (type === 'checkbox') {
+  setFormData((prevState) => ({
     ...prevState,
-    [name]: value
- }));
- };
+    [name]: checked,
+  }));
+  return;
+}
+
+setFormData((prevState) => ({
+  ...prevState,
+  [name]: value,
+}));
+};
 
  const handleFileChange = (e) => {
  const { name } = e.target;
@@ -75,102 +77,130 @@ const Form = () => {
  };
 
  return (
- <form onSubmit={handleSubmit}>
- <div>
- <label>Nombre del Evento:</label>
- <input 
- type="text"
- name="eventName"
- value={formData.eventName}
- onChange={handleChange}
- required 
- />
- </div>
-
- <div>
- <label>Descripción del Evento:</label>
- <textarea 
- name="eventDescription"
- placeholder='Máximo de 350 caracteres'
- maxLength="300"
- value={formData.eventDescription}
- onChange={handleChange}
- required 
- />
- </div>
-
- <div>
- <label>Fecha y Hora del Evento:</label>
- <input 
- type="datetime-local"
- name="eventDateTime"
- value={formData.eventDateTime} 
- onChange={handleChange} 
- required 
- />
- </div>
+<div className='form-page'>
+<form className="form-container" onSubmit={handleSubmit}>
+<div className="form-header">
+     <Link to="/">
+      <img
+          className="logo"
+          src="https://res.cloudinary.com/dj6gqmozm/image/upload/f_auto,q_auto/nqyo2gpte9c8kwsgqlbn"
+          alt="logo-culturalWave"
+           
+      />
+      </Link>
+</div>
 
 <div>
-        <label>Categoría del Evento:</label>
-        <select
-          name="audienceCategory"
-          value={formData.eventCategory}
+       <p className="form-title">Formulario para Solicitar Evento</p>
+     </div>
+
+     <div className="form-group">
+        <label>Nombre del Evento:</label>
+        <input
+          type="text"
+          name="eventName"
+          value={formData.eventName}
           onChange={handleChange}
           required
-        >
-          <option value="" disabled>
-            Selecciona una categoría del Evento
-          </option>
-          {[ "Música",
-        "Teatro y Danza",
-        "Cine",
-        "Arte y Exposiciones",
-        "Literarios",
-        "Conferencias",
-        "Talleres y Seminarios",
-        "Educación y Aprendizaje",
-        "Negocios y Emprendimiento",
-        "Deportes",
-        "Fitness y Salud",
-        "Deportes extremos",
-        "Artes Marciales",
-        "Familiares",
-        "Caridad y Voluntariado",
-        "Religión y Espiritualidad",
-        "Tecnología",
-        "Ciencia",
-        "Gastronomía",
-        "Bebidas",
-        "Moda",
-        "Estilo de Vida",
-        "Festivales y Carnavales",
-        "Celebraciones"].map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
+        />
       </div>
 
-      <div>
-        <label>Categoría de Público Objetivo:</label>
-        <select
-          name="audienceCategory"
-          value={formData.audienceCategory}
+      <div className="form-group">
+        <label>Descripción del Evento:</label>
+        <textarea
+          name="eventDescription"
+          placeholder="Máximo de 350 caracteres"
+          maxLength="350"
+          value={formData.eventDescription}
           onChange={handleChange}
           required
-        >
-          <option value="" disabled>
-            Selecciona una categoría de Audiencia
-          </option>
-          {["Todo Público", "13+", "16+", "18+", "Infantiles", "Adultos Mayores" , "LGBTQ+"].map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
+        />
       </div>
-    
+
+      <div className="form-group">
+        <label>Fecha y Hora del Evento:</label>
+        <input
+          type="datetime-local"
+          name="eventDateTime"
+          value={formData.eventDateTime}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div className="form-row">
+        <div className="form-group">
+          <label>Categoría del Evento:</label>
+          <select
+            name="eventCategory"
+            value={formData.eventCategory}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled>
+              Selecciona una categoría del Evento
+            </option>
+            {[
+              'Música',
+              'Teatro y Danza',
+              'Cine',
+              'Arte y Exposiciones',
+              'Literarios',
+              'Conferencias',
+              'Talleres y Seminarios',
+              'Educación y Aprendizaje',
+              'Negocios y Emprendimiento',
+              'Deportes',
+              'Fitness y Salud',
+              'Deportes extremos',
+              'Artes Marciales',
+              'Familiares',
+              'Caridad y Voluntariado',
+              'Religión y Espiritualidad',
+              'Tecnología',
+              'Ciencia',
+              'Gastronomía',
+              'Bebidas',
+              'Moda',
+              'Estilo de Vida',
+              'Festivales y Carnavales',
+              'Celebraciones',
+            ].map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label>Categoría de Público Objetivo:</label>
+          <select
+            name="audienceCategory"
+            value={formData.audienceCategory}
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled>
+              Selecciona una categoría de Audiencia
+            </option>
+            {[
+              'Todo Público',
+              '13+',
+              '16+',
+              '18+',
+              'Infantiles',
+              'Adultos Mayores',
+              'LGBTQ+',
+            ].map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
 
 
  <div>
@@ -192,7 +222,7 @@ const Form = () => {
     onChange={handleFileChange}
   />
   <div>
-  <label className="file-upload" htmlFor="flyerImage" for="flyerImage">Añadir un Flyer Promocional para el evento:</label>
+  <label className="file-upload" htmlFor="flyerImage" for="flyerImage">Añadir imagenes del Evento o Lugar:</label>
   <input 
     type="file" 
     accept=".jpg,.jpeg,.png" 
@@ -206,17 +236,9 @@ const Form = () => {
 </div>
 </div>
 
-<div >
-  <label>Añadir imágenes adicionales para el evento:</label>
-  <input type ="file" 
-  accept=".jpg,.jpeg,.png," 
-  multiple name= "additionalImages " 
-  onChange ={handleFileChange}/>
-</div >
-
 <div>
   <label>¿Es un evento online o presencial?</label>
-  <div>
+  <div className="radio-container">
     <label>
       <input
         type="radio"
@@ -241,20 +263,20 @@ const Form = () => {
 </div>
 
 <div className='social-media-links'>
-<label>Añadir enlaces a redes sociales:</ label ><br/>
-<label >Instagram :</label><input 
-type ="text" 
-name='instagramEventName' 
-placeholder='Instagram' 
-value ={formData.instagramEventName} 
-onChange ={handleChange}/><br/>
+    <label>Añadir enlaces a redes sociales:</ label ><br/>
+    <label >Instagram :</label><input 
+    type ="text" 
+    name='instagramEventName' 
+    placeholder='Instagram' 
+    value ={formData.instagramEventName} 
+    onChange ={handleChange}/><br/>
 
-<label >Facebook :</label ><input 
-type ='text' 
-name='facebookEventName' 
-placeholder='Facebook'
-value ={formData.facebookEventName }
-onChange ={handleChange}/><br/>
+    <label >Facebook :</label ><input 
+    type ='text' 
+    name='facebookEventName' 
+    placeholder='Facebook'
+    value ={formData.facebookEventName }
+    onChange ={handleChange}/><br/>
 </div>
 
 
@@ -267,19 +289,37 @@ onChange ={handleChange}/><br/>
   </select>
 </div>
 
+<div className="checkbox-container">
+       <label>
+         <input
+           type="checkbox"
+           name="isFree"
+           checked={formData.isFree}
+           onChange={handleChange}
+         />
+         ¿Es un evento gratis?
+       </label>
+     </div>
 
-<div><label>Precio del Evento en USD:</label><input 
-type ='number' 
-min ='0' 
-step ='any' 
-max='' 
-name='priceUSD'
-value= {formData.priceUSD} 
-onChange= {handleChange} 
-required/> 
-</div>
+     
+     {!formData.isFree && (
+       <div>
+         <label>Precio del Evento en USD:</label>
+         <input
+           type="number"
+           min="0"
+           step="any"
+           name="priceUSD"
+           value={formData.priceUSD}
+           onChange={handleChange}
+           required
+         />
+       </div>
+     )}
+
 
 <button type ="submit">Enviar Solicitud</button></form >  
+</div>
  );
 };
 
