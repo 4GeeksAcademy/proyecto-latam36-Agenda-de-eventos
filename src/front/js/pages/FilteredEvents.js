7e65c2b9c72b2b5e63db61485ec4838cc7f7f85f
martincorestein <103467287+martincorestein@users.noreply.github.com>
2025-01-10 07:46:00 +0000

    configurado FilteredEvents.js para que cargue correctamente los eventos seleccionando categorias y obteniendo grupo de categorias desde el link

--- src/front/js/pages/FilteredEvents.js
+++ src/front/js/pages/FilteredEvents.js
@@ -11,12 +11,13 @@ const FilteredEvents = () => {
   const { store } = useContext(Context);
   const { category } = useParams();
   const [filteredEvents, setFilteredEvents] = useState({});
-  const [selectedCategory, setSelectedCategory] = useState("Todos");
+  const [selectedCategory, setSelectedCategory] = useState(""); 
+  const [breadcrumbsTitle, setBreadcrumbsTitle] = useState(filtersConfig[category]?.title || "Todos");
   const [isOnline, setIsOnline] = useState(null);
   const [selectedPrice, setSelectedPrice] = useState("Todos");
   const [ageClassification, setAgeClassification] = useState("Todos");
   const [error, setError] = useState(null);
-  const [initialLoad, setInitialLoad] = useState(true); // Para manejar la carga inicial
+  const [initialLoad, setInitialLoad] = useState(true); 
 
   // Obtener las categorías desde el filterKey
   const categories = filtersConfig[category]?.filter?.category.split(", ") || [];
@@ -24,7 +25,10 @@ const FilteredEvents = () => {
   const fetchFilteredEvents = async (queryCategories) => {
     try {
       const queryParams = new URLSearchParams();
-      queryCategories.forEach(cat => queryParams.append("category", cat));
+      if (queryCategories.length > 0) {
+        queryCategories.forEach(cat => queryParams.append("category", cat));
+      }
+      queryParams.append("status", "approved"); 
       if (isOnline !== null) queryParams.append("is_online", isOnline);
       if (selectedPrice !== "Todos") queryParams.append("price_type", selectedPrice.toLowerCase() === "gratis" ? "free" : "paid");
       if (ageClassification !== "Todos") queryParams.append("age_classification", ageClassification);
@@ -42,13 +46,14 @@ const FilteredEvents = () => {
 
       const events = await response.json();
 
-      // Agrupar eventos por categoría
       const groupedEvents = events.reduce((acc, event) => {
-        const eventCategories = event.category.split(', ');
-        eventCategories.forEach(cat => {
-          if (!acc[cat]) acc[cat] = [];
-          acc[cat].push(event);
-        });
+        if (event.status === "approved") { 
+          const eventCategories = event.category.split(', ');
+          eventCategories.forEach(cat => {
+            if (!acc[cat]) acc[cat] = [];
+            acc[cat].push(event);
+          });
+        }
         return acc;
       }, {});
 
@@ -68,69 +73,72 @@ const FilteredEvents = () => {
 
   useEffect(() => {
     if (!initialLoad) {
-      fetchFilteredEvents([selectedCategory]);
+      fetchFilteredEvents(selectedCategory === "" || selectedCategory === "Todos" ? [] : [selectedCategory]);
+      // Actualizar el título del breadcrumbs al seleccionar una categoría
+      setBreadcrumbsTitle(selectedCategory !== "" && selectedCategory !== "Todos" ? selectedCategory : filtersConfig[category]?.title || "Todos");
     }
   }, [selectedCategory, isOnline, selectedPrice, ageClassification, store.token]);
 
   return (
     <>
       <Navbar />
-      <Breadcrumbs />
+      <Breadcrumbs eventName={breadcrumbsTitle} />
 
-    <div className="filtered-events-page">
-      <h2>Eventos en la categoría: {filtersConfig[category]?.title || "Todos"}</h2>
-      <EventFilters 
-        selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}
-        isOnline={isOnline}
-        setIsOnline={setIsOnline}
-        selectedPrice={selectedPrice}
-        setSelectedPrice={setSelectedPrice}
-        ageClassification={ageClassification}
-        setAgeClassification={setAgeClassification}
-      />
-      <div className="events-grid">
-        {error ? (
-          <p>Error al cargar los eventos: {error}</p>
-        ) : Object.keys(filteredEvents).length > 0 ? (
-          Object.entries(filteredEvents).map(([cat, events]) => (
-            <div key={cat} className="category-section">
-              <h3>{cat}</h3>
-              <div className="events-cards">
-                {events.slice(0, 5).map((event) => (
-                  <div className="event-card" key={event.id}>
-                    <img
-                      src={
-                        event.flyer_img_url ||
-                        "https://via.placeholder.com/800x400?text=No+Image"
-                      }
-                      alt={event.event_name}
-                    />
-                    <div className="event-info">
-                      <h3>{event.event_name}</h3>
-                      <p>{new Date(event.event_date).toLocaleDateString("es-ES")}</p>
+      <div className="filtered-events-page">
+        <h2>Eventos en la categoría: {breadcrumbsTitle}</h2>
+        <EventFilters 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          isOnline={isOnline}
+          setIsOnline={setIsOnline}
+          selectedPrice={selectedPrice}
+          setSelectedPrice={setSelectedPrice}
+          ageClassification={ageClassification}
+          setAgeClassification={setAgeClassification}
+        />
+        <div className="events-grid">
+          {error ? (
+            <p>Error al cargar los eventos: {error}</p>
+          ) : Object.keys(filteredEvents).length > 0 ? (
+            Object.entries(filteredEvents).map(([cat, events]) => (
+              <div key={cat} className="category-section">
+                <h3>{cat}</h3>
+                <div className="events-cards">
+                  {events.slice(0, 5).map((event) => (
+                    <div className="event-card" key={event.id}>
+                      <img
+                        src={
+                          event.flyer_img_url ||
+                          "https://via.placeholder.com/800x400?text=No+Image"
+                        }
+                        alt={event.event_name}
+                      />
+                      <div className="event-info">
+                        <h3>{event.event_name}</h3>
+                        <p>{new Date(event.event_date).toLocaleDateString("es-ES")}</p>
+                      </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
+                {events.length > 5 && (
+                  <button 
+                    className="see-more-button"
+                    onClick={() => {
+                      setSelectedCategory(cat);
+                      setInitialLoad(false); 
+                      setBreadcrumbsTitle(cat); 
+                    }}
+                  >
+                    Ver más
+                  </button>
+                )}
               </div>
-              {events.length > 5 && (
-                <button 
-                  className="see-more-button"
-                  onClick={() => {
-                    setSelectedCategory(cat);
-                    setInitialLoad(false); // Asegurar que no se ejecute la carga inicial nuevamente
-                  }}
-                >
-                  Ver más
-                </button>
-              )}
-            </div>
-          ))
-        ) : (
-          <p>No hay eventos disponibles en esta categoría.</p>
-        )}
+            ))
+          ) : (
+            <p>No hay eventos disponibles en esta categoría.</p>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
